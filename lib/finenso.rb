521c7f5a1e18ad95f5d9cb87e28991f160c98f90
Jithu Gopal <jithug87@gmail.com>
2014-03-07 22:59:40 +0530

    adding right border to address box

--- lib/finenso.rb
+++ lib/finenso.rb
@@ -11,12 +11,14 @@ module Finenso
           bounding_box([0, cursor - 20], width: 110) do
             font "Helvetica"
             font_size 9
-            indent 5 do
+            indent 0, 10 do
               text "Nilenso Software LLP", align: :right, style: :bold
               text "#147, 10th Cross", align: :right
               text "Indiranagar Stage 1", align: :right
               text "Bangalore, India, 560038", align: :right
             end
+            stroke_color "F2136A"
+            stroke_vertical_line bounds.bottom, bounds.top, at: bounds.right
           end
         end
       end
