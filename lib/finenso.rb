95d211e6fdc26fbd20d19d1b709d506c9457989b
Jithu Gopal <jithug87@gmail.com>
2014-03-07 17:28:44 +0530

    Forex Topup task - adding watermark for letterhead

--- lib/finenso.rb
+++ lib/finenso.rb
@@ -3,8 +3,12 @@ require "prawn"
 module Finenso
   module Forex
     class Topup
+      ASSETS_DIR = File.expand_path("../../assets", __FILE__)
+      OUTPUT_DIR = File.expand_path("../../target", __FILE__)
       def generate_pdf
-        Prawn::Document.generate("letter.pdf")
+        Prawn::Document.generate("#{OUTPUT_DIR}/letterhead.pdf") do
+          image "#{ASSETS_DIR}/nilenso-bg.png", width: 220, at: [410, cursor + 110]
+        end
       end
     end
   end
