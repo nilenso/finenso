d46101f573edba21f00207b5af37c6dbacf255d5
Jithu Gopal <jithug87@gmail.com>
2014-03-07 19:08:09 +0530

    adding address to letterhead

--- lib/finenso.rb
+++ lib/finenso.rb
@@ -8,6 +8,16 @@ module Finenso
       def generate_pdf
         Prawn::Document.generate("#{OUTPUT_DIR}/letterhead.pdf") do
           image "#{ASSETS_DIR}/nilenso-bg.png", width: 220, at: [410, cursor + 110]
+          bounding_box([0, cursor - 20], width: 110) do
+            font "Helvetica"
+            font_size 9
+            indent 5 do
+              text "Nilenso Software LLP", align: :right, style: :bold
+              text "#147, 10th Cross", align: :right
+              text "Indiranagar Stage 1", align: :right
+              text "Bangalore, India, 560038", align: :right
+            end
+          end
         end
       end
     end
