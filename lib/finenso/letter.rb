45cab517e859cfc79f86cb2757b486e0242cfb5a
Jithu Gopal <jithug87@gmail.com>
2014-03-10 18:41:13 +0530

    adding PT Sans font for letter head

--- lib/finenso/letter.rb
+++ lib/finenso/letter.rb
@@ -4,6 +4,10 @@ module Finenso
   class Letter
     def generate_pdf(body)
       document = Prawn::Document.new
+      document.font_families.update("PT Sans" => {
+        normal: "#{Finenso.config.assets_dir}/fonts/PT_Sans-Web-Regular.ttf",
+        bold: "#{Finenso.config.assets_dir}/fonts/PT_Sans-Web-Bold.ttf"
+      })
       prepare_letterhead(document)
       document.move_down 200
       prepare_content(document, body)
@@ -11,15 +15,19 @@ module Finenso
     end
 
     def prepare_letterhead(document)
-      document.image "#{Finenso.config.assets_dir}/nilenso-bg.png", width: 220, at: [410, document.cursor + 110]
-      document.bounding_box([0, document.cursor - 20], width: 110) do
-        document.font "Helvetica"
-        document.font_size 9
+      document.image "#{Finenso.config.assets_dir}/images/nilenso-bg.png", width: 220, at: [410, document.cursor + 110]
+      document.bounding_box([0, document.cursor - 20], width: 130) do
+        document.font_size 10
         document.indent 0, 10 do
-          document.text "Nilenso Software LLP", align: :right, style: :bold
-          document.text "#147, 10th Cross", align: :right
-          document.text "Indiranagar Stage 1", align: :right
-          document.text "Bangalore, India, 560038", align: :right
+          document.font("PT Sans") do
+            document.text "Nilenso Software LLP", align: :right, style: :bold
+            document.text "#147, 10th Cross", align: :right
+            document.text "Indiranagar Stage 1", align: :right
+            document.text "Bangalore, India, 560038", align: :right
+            document.move_down 8
+            document.text "+91 80 4204 2671", align: :right
+            document.text "moshimoshi@nilenso.com", align: :right
+          end
         end
         document.stroke_color "F2136A"
         bounds = document.bounds
