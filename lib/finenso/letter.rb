7bd883da8035783fd496d85e369777ea871bd74c
Jithu Gopal <jithug87@gmail.com>
2014-03-10 17:36:05 +0530

    refactoring out methods from Prawn::Document implicit block

--- lib/finenso/letter.rb
+++ lib/finenso/letter.rb
@@ -3,26 +3,35 @@ require "prawn"
 module Finenso
   class Letter
     def generate_pdf(body)
-      Prawn::Document.generate("#{Finenso.config.target_dir}/Forex cards issue request.pdf") do
-        image "#{Finenso.config.assets_dir}/nilenso-bg.png", width: 220, at: [410, cursor + 110]
-        bounding_box([0, cursor - 20], width: 110) do
-          font "Helvetica"
-          font_size 9
-          indent 0, 10 do
-            text "Nilenso Software LLP", align: :right, style: :bold
-            text "#147, 10th Cross", align: :right
-            text "Indiranagar Stage 1", align: :right
-            text "Bangalore, India, 560038", align: :right
-          end
-          stroke_color "F2136A"
-          stroke_vertical_line bounds.bottom, bounds.top, at: bounds.right
+      document = Prawn::Document.new
+      prepare_letterhead(document)
+      document.move_down 200
+      prepare_content(document, body)
+      document.render_file("#{Finenso.config.target_dir}/Forex cards issue request.pdf")
+    end
+
+    def prepare_letterhead(document)
+      document.image "#{Finenso.config.assets_dir}/nilenso-bg.png", width: 220, at: [410, document.cursor + 110]
+      document.bounding_box([0, document.cursor - 20], width: 110) do
+        document.font "Helvetica"
+        document.font_size 9
+        document.indent 0, 10 do
+          document.text "Nilenso Software LLP", align: :right, style: :bold
+          document.text "#147, 10th Cross", align: :right
+          document.text "Indiranagar Stage 1", align: :right
+          document.text "Bangalore, India, 560038", align: :right
         end
-        move_down 200
-        bounding_box([100, 500], width: 420) do
-          today = Date.current
-          text today.strftime("%B #{today.day.ordinalize} %Y"), align: :right
-          text body
-        end
+        document.stroke_color "F2136A"
+        bounds = document.bounds
+        document.stroke_vertical_line bounds.bottom, bounds.top, at: bounds.right
+      end
+    end
+
+    def prepare_content(document, body = "")
+      document.bounding_box([100, 500], width: 420) do
+        today = Date.current
+        document.text today.strftime("%B #{today.day.ordinalize} %Y"), align: :right
+        document.text body
       end
     end
   end
