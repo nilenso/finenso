b2db652761b079b22d7e50f8f42194c45188e6ac
Jithu Gopal <jithug87@gmail.com>
2014-03-13 19:33:46 +0530

    [#1] use employee yml instead of csv for generating topup request

--- Rakefile
+++ Rakefile
@@ -1,6 +1,17 @@
 require File.expand_path("../lib/finenso.rb", __FILE__)
 
 desc "Generate pdf for forex topup"
-task :forex_topup do
-  Finenso::Forex.new.topup_request
+task :forex_topup, [:employee, :amount] do |t, args|
+  if verify(args[:employee], "Missing employee argument.") && verify(args[:amount], "Missing amount to be credited.")
+    Finenso::Forex.new.topup_request(args[:employee], args[:amount].to_i)
+  end
+end
+
+def verify(option, msg)
+  if option
+    true
+  else
+    puts msg
+    false
+  end
 end
--- lib/finenso/forex.rb
+++ lib/finenso/forex.rb
@@ -1,40 +1,31 @@
 module Finenso
   class Forex
-    def topup_request
-      topup_request_file = File.join(Finenso.config.data_dir, "topup_request.csv")
-      return unless verify(topup_request_file)
-      CSV.foreach(topup_request_file, :col_sep => ",", headers: true) do |row|
-        eefc_account_number = row[0]
-        card_number = row[1]
-        card_holder_name = row[2]
-        amount = row[3].to_i
-        body = <<-EOS
-        To
-        The Manager
-        HDFC Bank Ltd
-        100 Feet Road Branch,
-        Bangalore - 560038
-
-
-        Dear Sir,
-
-        Subject: Transfer USD #{amount} from EEFC account to Forex card #{card_number}
-
-        Requesting you to load the Forex card #{card_number} for #{card_holder_name} using the EEFC account #{eefc_account_number} with USD #{amount} (USD #{amount.to_words.titleize}).
-
-        Thanking you
-        EOS
-        Finenso::Letter.new.generate_pdf(body)
+    def topup_request(employee_name, amount)
+      employee = Employee.find_by_name(employee_name)
+      unless employee
+        puts "#{employee_name} does not exist. Please make sure your employees.yml has #{employee_name}" unless employee
+        return
       end
-    end
+      eefc_account_number = employee["eefc_account_number"]
+      card_number = employee["card_number"]
+      first_name = employee["first_name"]
+      body = <<-EOS
+      To
+      The Manager
+      HDFC Bank Ltd
+      100 Feet Road Branch,
+      Bangalore - 560038
 
-    def verify(file)
-      if File.exists? file
-        true
-      else
-        puts "Missing required csv file. Please create it from the templates present in the data folder."
-        false
-      end
+
+      Dear Sir,
+
+      Subject: Transfer USD #{amount} from EEFC account to Forex card #{card_number}
+
+      Requesting you to load the Forex card #{card_number} for #{first_name} using the EEFC account #{eefc_account_number} with USD #{amount} (USD #{amount.to_words.titleize}).
+
+      Thanking you
+      EOS
+      Finenso::Letter.new.generate_pdf(body)
     end
   end
 end
